e38d95c186925ec68c8f32a558345284eba7cda5
jio-kisou <jiotto.ppp.ntd@gmail.com>
2018-12-10 23:00:07 +0900

    add new data

--- read_json.py
+++ read_json.py
@@ -3,11 +3,11 @@ import json
 if __name__ == "__main__":
     j = 0
     lines = []
-    with open("data.json", "r") as f:
+    with open("programs_training.json", "r") as f:
         for line in f:
             lines.append(line)
             j += 1
-            if j > 1000:
+            if j > 100:
                 break
     with open("data1.json", "w") as f1:
         f1.writelines(lines)
